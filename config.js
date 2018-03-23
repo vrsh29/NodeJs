180c4413b2a9341e52c5175954f59af0c7008a29
varsha nagaraj <varsha.m.nagaraj@accenture.com>
2018-03-23 07:28:53 +0530

    facebook auth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongod': 'mongodb://localhost:27017/conFusion'
+    'mongod': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: 160431257989691,
+        clientSecret: '94f3704be775edb84a548dca9ae2b081'
+    }
 }
--- routes/users.js
+++ routes/users.js
@@ -86,4 +86,16 @@ router.get('/logout', (req,res) => {
   }
 });
 
+router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
+  if(req.user) {
+     var token = authenticate.getToken({ _id: req.user._id });
+     res.statusCode = 200;
+     res.setHeader("Content-type", "application/json");
+     res.json({
+     success: true,
+     token: token,
+     status: "You are Successfully logged in!"});
+  }
+  });
+
 module.exports = router;
